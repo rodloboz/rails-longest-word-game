5a31381000bf0b86761db801b1c0c78ccc51a17d
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-02-13 16:48:33 +0000

    added session score and difficulty level

--- app/controllers/longestword_controller.rb
+++ app/controllers/longestword_controller.rb
@@ -2,13 +2,14 @@ require 'longest_word'
 
 class LongestwordController < ApplicationController
   def game
+    params[:difficulty].nil? ? @grid_size = 20 : @grid_size = params[:difficulty].to_i
     @start_time = Time.now
-    @grid = generate_grid(20)
+    @grid = generate_grid(@grid_size)
 
   end
 
   def score
-
     @results = run_game(params[:attempt], params[:grid].chars, params[:start_time].to_datetime, Time.now)
+    session[:score].nil? ? session[:score] = 0 : session[:score] += @results[:score]
   end
 end
